f9cdaa59951153106d86577eb37def241b9837e0
0xDracarys <shubhambhaskr123@gmail.com>
2026-07-12 22:33:53 +0530

    fix: remove standalone output for Vercel compat, fix build script, fix render start cmd

--- lfs-learning-platform/next.config.js
+++ lfs-learning-platform/next.config.js
@@ -1,12 +1,12 @@
 // @ts-check
-const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  outputFileTracingRoot: path.resolve(__dirname),
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
